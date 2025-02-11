f7e0a6ff43bc2e1347d2ab9aa764285809bef0e4
mraniket <aashinde20@gmail.com>
2025-02-11 22:42:29 +0530

    merging the main

--- qpgrocery/src/main/java/com/qpassessment/qpgrocery/dao/UserRepository.java
+++ qpgrocery/src/main/java/com/qpassessment/qpgrocery/dao/UserRepository.java
@@ -8,9 +8,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
-<<<<<<< Updated upstream
-	//sout hello
-=======
-    //as
->>>>>>> Stashed changes
+
 }
